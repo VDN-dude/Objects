5e9ba7be59c566eb98d2226b715b7f836bdb05a0
Yuri-Vanderon <garanovich2@gmail.com>
2023-01-16 12:39:43 +0300

    1

--- src/ConsoleApplication.java
+++ src/ConsoleApplication.java
@@ -3,6 +3,7 @@ public class ConsoleApplication implements Application{
     Reader reader = new ConsoleReader();
     Writer writer = new ConsoleWriter();
     OperationStorage storage = new InMemoryOperationStorage();
+    private boolean t = true;
     @Override
     public void run() {
         while (true) {
@@ -18,22 +19,33 @@ public class ConsoleApplication implements Application{
             storage.save(result);
             writer.writeln("Result: " + result.getResult());
 
-            writer.writeln("Display history? [Y]es , [N]o : ");
-            String history = reader.readString();
-            switch (history){
-                case "n":
-                    break;
-                case "y":
-                    storage.findAll();
+            while (t) {
+                writer.writeln("Display history? [Y]es , [N]o : ");
+                String history = reader.readString();
+                switch (history) {
+                    case "n":
+                        t = false;
+                        break;
+                    case "y":
+                        storage.findAll();
+                        t = false;
+                    default:
+                        writer.writeln("Selection not found, try again.");
+                }
             }
 
-            writer.writeln("Want to continue? [Y]es , [N]o : ");
-            String replay = reader.readString();
-            switch (replay) {
-                case "n":
-                    return;
-                case "y":
-                    break;
+            while (!t) {
+                writer.writeln("Want to continue? [Y]es , [N]o : ");
+                String replay = reader.readString();
+                switch (replay) {
+                    case "n":
+                        return;
+                    case "y":
+                        t = true;
+                        break;
+                    default:
+                        writer.writeln("Selection not found, try again.");
+                }
             }
         }
     }
