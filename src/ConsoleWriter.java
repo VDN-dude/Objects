f19c1087cd0594b98a130b28064ad8e4dafbd3dd
Yuri-Vanderon <garanovich2@gmail.com>
2023-01-19 15:07:13 +0300

    1

--- src/ConsoleWriter.java
+++ src/ConsoleWriter.java
@@ -3,8 +3,6 @@ import java.util.Arrays;
 public class ConsoleWriter {
     private String message;
     public void write(String message) {System.out.print(message);}
-    public void writeln(String message) {
-        System.out.println(message);
-    }
+    public void writeln(String message) {System.out.println(message);}
 
 }
