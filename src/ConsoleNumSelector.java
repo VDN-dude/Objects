af5d3be83caec9bc7feca13c3b4fdac82b9d25bb
Yuri-Vanderon <garanovich2@gmail.com>
2023-02-06 19:11:51 +0300

    1

--- src/ConsoleNumSelector.java
+++ src/ConsoleNumSelector.java
@@ -3,15 +3,10 @@ public class ConsoleNumSelector {
     private final ConsoleReader reader = new ConsoleReader();
     private final ConsoleWriter writer = new ConsoleWriter();
 
-    public ConsoleNumSelector() {
-    }
-
     public double selectNum1(){
         writer.writeln("Enter number 1: ");
         return reader.readDouble();
-
     }
-
     public double selectNum2(){
         writer.writeln("Enter number 2: ");
         return reader.readDouble();
