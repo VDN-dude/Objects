a86595d413291d5fb97c6129ccb47dd79cca8fb1
Yuri-Vanderon <garanovich2@gmail.com>
2023-03-06 13:50:00 +0300

    returned to previous version

--- src/by/tms/console/ConsoleNumSelector.java
+++ src/by/tms/console/ConsoleNumSelector.java
@@ -1,8 +1,16 @@
 package by.tms.console;
 
 import by.tms.console.util.ConsoleReader;
-
 public class ConsoleNumSelector {
-    private final ConsoleReader reader = new ConsoleReader();
-    public double selectNum() {return reader.readDouble();}
+    private ConsoleReader reader = new ConsoleReader();
+    public double selectNum() {
+        while (true) {
+            try {
+                return reader.readDouble();
+            } catch (RuntimeException e) {
+                System.out.println("Num was entered incorrect, try again");
+                reader = new ConsoleReader();
+            }
+        }
+    }
 }
