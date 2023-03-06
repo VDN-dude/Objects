8dd8ee29327345d6128d76ea91a15ac9890d8bb7
Yuri-Vanderon <garanovich2@gmail.com>
2023-03-06 13:51:42 +0300

    deleted

--- src/by/tms/validator/DoubleValidator.java
+++
@@ -1,8 +0,0 @@
-package by.tms.validator;
-
-import java.util.regex.Pattern;
-
-public class DoubleValidator {
-    private static final Pattern DOUBLE = Pattern.compile("^\\d[.\\d]*$");
-    public boolean isValidDouble(String num){return DOUBLE.matcher(num).matches();}
-}
