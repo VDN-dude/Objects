fc84bb54f8426f682c235b7a649427ad27bdbaa9
Yuri-Vanderon <garanovich2@gmail.com>
2023-03-12 12:34:10 +0300

    renamed class

--- src/by/tms/validator/CalculatorValidator.java
+++ src/by/tms/validator/CalculatorValidator.java
@@ -2,10 +2,10 @@ package by.tms.validator;
 
 import java.util.regex.Pattern;
 
-public class Validator {
+public class CalculatorValidator {
     private static final Pattern NUM = Pattern.compile("^\\d[.\\d]*$");
     private static final Pattern OperationType = Pattern.compile("^(SUM)\\b|(SUB)\\b|(MUL)\\b|(DIV)\\b$");
 
-    public boolean isValidDouble(String num){return NUM.matcher(num).matches();}
+    public boolean isValidNum(String num){return NUM.matcher(num).matches();}
     public boolean isValidOperationType(String type){return OperationType.matcher(type).matches();}
 }
