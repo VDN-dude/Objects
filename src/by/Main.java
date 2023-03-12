c0fc56e74ffbc924de8490a5d26de70ada99a24e
Yuri-Vanderon <garanovich2@gmail.com>
2023-03-12 12:44:31 +0300

    changed console to http

--- src/by/Main.java
+++ src/by/Main.java
@@ -2,15 +2,9 @@ package by;
 
 import by.tms.http.HttpApplication;
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args){
         Application app = new HttpApplication();
-        try {
-            app.run();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        app.run();
     }
 }
